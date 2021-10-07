d4c0e088b1c955d5a621f18f3b4ceeb452235ffd
jackransome <31069425+jackransome@users.noreply.github.com>
2021-10-08 00:36:07 +1100

    Added art for main character, drone, and fire. started object class, added more planning

--- SFML_Engine/Game.cpp
+++ SFML_Engine/Game.cpp
@@ -31,26 +31,91 @@ Game::Game(sf::RenderWindow* pwindow) {
 
 // GAME plan
 //
+// SETTING:
 // snowing and empty, stone things rise out of the ground, activated by pressure pads and levers
-// are gives you an augmented reality headset, allowing you to access the main gameplay of the game
+// at some point you find an augmented reality headset, allowing you to access the main gameplay of the game (before this point sometimes you are randomly attacked / damaged
 // some things follow you out of AR
-// AR things used to open up real world areas
-// Dying in AR = dying irl, like killed by the guardians in halo, maybe a stone pillar will rise out of the snow and laser beam you
+// 
+// 
 // cinematic bits that show you fighting nothing
 // sometimes the AR will flicker
 // some effects in game will cause noisyness in AR, or cut it out
-// hovering drone indicates when virtual enemies are nearby
+// 
 // real enemies are revealed at somepoint, first appear in AR but remain when taking your headset off
+// first AR encounter: put on, screen fased to black, then to black and white, then to colour with AR stuff
 // 
 // GOAL: explore, upgrade gear, learn more and escape
 // 
 // MUSIC:
 // 
-// intro track will be track 1 from my ep
-// another track will be triasic period
-// use drone track for some death sounds and maybe ambience
-// untitled 149 from FL for happy victory or chill bustling area
+//		intro track will be track 1 from my ep
+//		another track will be triasic period
+//		use drone track for some death sounds and maybe ambience
+//		untitled 149 from FL for happy victory or chill bustling area
+//		tracks w breaks for in AR
 // 
+// MECHANICS:
+// 
+//  - GENERAL
+//		AR things used to open up real world areas
+//		hovering drone indicates when virtual enemies are nearby
+//		mostfights will be in AR
+//		puzzles will have some AR component most of the time
+//		AR headset will have battery, when it runs out and enemies come you are defenseless
+// 
+//  - COMBAT:
+//		you can dash twice and then have a cooldown
+//		can place down pylons, or marks, when 2 have been place a beem appears between the two for a split second and damages all enemies inside (beam: fire? electric?)
+//		Dying in AR = dying irl, like killed by the guardians in halo, maybe a stone pillar will rise out of the snow and laser beam you
+//		
+//	- hovering drone:
+//		indicates when virtual enemies are nearby
+//		also indicates nearby things you can interact with
+//		may play a role in other puzzles, leaving you vulerable it you make it stay in one place for a task
+//		may also become possessed in some parts of the story, going yellow?
+//		
+// 
+//	- INTERRACTION AND INVENTORY
+//		F to interract, must hold F to use things, fills up a box or bar, instead of just clicking
+//		E to open inventory
+//		Interacting with specific thing can open and close doors, as well as spawn enemies and structures
+// 
+//  - ENVIRONMENTS:
+//		Outdoors: snow and footprints
+//		Indoors: stone walls and floor
+// 
+// GRAPHICS:
+//		Footprints and blood will be left on snow, fading away over time
+// 
+// TODO:
+//		Global interfaces:
+//			Graphics
+//			Audio
+//		Object class and inheritance and collection
+//			bounding boxes
+//			has z
+//			generic drawing method
+//			(for player, enemies, walls, stairs, items, Particle etc)
+//			Object collection may need subsets that are loopable, for particles, enemies, etc
+// 
+//		Decoration class and inheritance and collection:
+//			has xy and spritesheet/image
+//			
+//			(for blood, footsteps, burn marks)
+// 
+//		Particle class with inheritance and collection
+//			Inherits object
+//			has xyz and spritesheet/image
+//			obeys same system as players and enemies in terms of elevated areas / cliffs
+//			some should spawn decoration when landing / despawning
+//			be able to remain on ground for set period of time, or disapear on impact
+// 
+//		Figure out system for elevated areas and stairs:
+//			stairs: faster to walk down, slower to walk up
+//			ledges/cliffs: falling off (moving off a raises platform(just a square) and not onto stairs, changes your position to a much greater y and adds equal z
+//			then gravity and z is a thing
+//
+//
 
 
 
@@ -103,6 +168,7 @@ void Game::Draw() {
 
 
 	spriteCollection.addAbsoluteCircleDraw(inputManager.mouseX, inputManager.mouseY, 50, inputManager.mouseY, sf::Color(255, 0, 0, 255));
+
 
 	spriteSheet1.run();
 	spriteSheet1.draw(x, y, y);
